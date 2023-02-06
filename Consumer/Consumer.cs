fb6c331e47cbc6506be76a3aefb1df8e76efc560
hossein193 <hosseingolmohammadi193201@gmail.com>
2023-02-06 22:09:10 +0330

    code

---
+++ Consumer/Consumer.cs
@@ -0,0 +1,34 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Consumer
+{
+    public class Consumer
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                var queue = "QueueTest";
+
+                channel.QueueDeclare(queue, false, false, false, null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    string data = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(data);
+                };
+
+                channel.BasicConsume(queue, true, consumer);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Pubilsh/Sender.cs
@@ -0,0 +1,47 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Producer
+{
+    public class Sender
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                var queue = "QueueTest";
+                channel.QueueDeclare(queue, false, false, false, null);
+
+                string m = "frist app with rabbitmq";
+
+                var body = Encoding.UTF8.GetBytes(m);
+
+                channel.BasicPublish("", queue, null, body);
+
+                Console.WriteLine(m);
+            }
+            Console.WriteLine("Press any key");
+            Console.ReadKey();
+        }
+
+        #region MyRegion
+
+        public void SenderQueue(object message, string queue, string exchange = "", string hostName = "localHost")
+        {
+            var factory = new ConnectionFactory() { HostName = hostName };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue, false, false, false, null);
+
+                var body = Encoding.UTF8.GetBytes((char[])message);
+                channel.BasicPublish(exchange, queue, null, body);
+            }
+        }
+        #endregion
+
+    }
+}
